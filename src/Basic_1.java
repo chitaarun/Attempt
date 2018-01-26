3a24aa1c4095f426d817ac89ff920ae4cb052856
chitaarun <chitaarun@gmail.com>
2018-01-25 23:06:36 -0500

    attempt program added

---
+++ src/Basic_1.java
@@ -0,0 +1,26 @@
+
+public class Basic_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		//comment
+		
+System.out.println("Attempt to remote log in");
+
+		//variable declare
+		
+// we want to add 5,7 
+
+int a=5;
+int b=7;
+ int sum=a+b;
+ 
+ System.out.println("sun is " +7);
+ 
+
+	}
+
+}
